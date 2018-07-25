2b3406e6e59cc004a2cf2473ace35c0badc5fa26
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-07-25 21:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+ const drivers = [
+    { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
+    { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
+    { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
+    { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
+    { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
+  ];
+  
+  let logDriverNames=function(array){
+    return array.forEach(function(array){
+      console.log(array.name);
+    });
+  };
+  
+  let logDriversByHometown=function(array , hometown){
+    return array.forEach(function(array){
+      if(array.hometown===hometown)
+      console.log(array.name);
+      
+    });
+  };
+
+  
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (first,second) {
+    return first.revenue - second.revenue;
+  });
+};
